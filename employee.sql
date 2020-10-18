38e1bd292d8836e5cac5b9487a8870fb1816aaa4
alfredo pedroza <alfredopedrozahernandez13@gmail.com>
2020-10-17 20:09:38 -0700

    added all files..still needs fixing

---
+++ employee.sql
@@ -0,0 +1,69 @@
+DROP DATABASE IF EXISTS employees;
+CREATE database employees;
+
+USE employees;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) DEFAULT '' NOT NULL,
+  PRIMARY KEY (id)
+
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT,
+  PRIMARY KEY (id)
+);
+
+-- * **department**:
+INSERT INTO department (name) VALUE ("Sales");
+INSERT INTO department (name) VALUE ("Support");
+INSERT INTO department (name) VALUE ("Engineer");
+
+-- * **role**:
+INSERT INTO role (title, salary, department_id) 
+VALUE ("Sales Lead", 70000, 1);
+INSERT INTO role (title, salary, department_id) 
+VALUE ("Sales Rep", 55000, 1);
+INSERT INTO role (title, salary, department_id) 
+VALUE ("Support Lead", 40000, 2);
+INSERT INTO role (title, salary, department_id) 
+VALUE ("Email Support", 60000, 2);
+INSERT INTO role (title, salary, department_id) 
+VALUE ("Tech Support", 90000, 2);
+INSERT INTO role (title, salary, department_id) 
+VALUE ("Senior Engineer", 120000, 3);
+INSERT INTO role (title, salary, department_id) 
+VALUE ("Engineer", 80000, 3);
+
+--   * **id** - INT PRIMARY KEY
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Austin", "Asher", 1, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Sly", "Jones", 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Chris", "Payton", 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Michael", "Roy", 3, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Kyle", "Miles", 4, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Jordan", "Smith", 5, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Ashley", "Santos", 6, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Bertha", "Hernandez", 7, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Raul", "Ramirez", 7, 3);
---
+++ server.js
@@ -0,0 +1,189 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "employees",
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  initPrompt();
+});
+
+function initPrompt() {
+  inquirer
+    .prompt({
+      name: "prompt",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View all employees",
+        "View all roles",
+        "View all departments",
+        "Add an employee",
+        "Add a department",
+        "Add a role",
+        "Update an employee role",
+        "Quit",
+      ],
+    })
+    .then(function (response) {
+      switch (response.prompt) {
+        case "View all employees":
+          viewAllEmployees();
+          break;
+        case "View all roles":
+          viewAllRoles();
+          break;
+        case "View all departments":
+          viewAllDepartments();
+          break;
+        case "Add an employee":
+          addEmployee();
+          break;
+        case "Add a department":
+          addDepartment();
+          break;
+        case "Add a role":
+          addRole();
+          break;
+        case "Update an employee role":
+          updateEmployee();
+          break;
+        case "Quit":
+          process.exit();
+      }
+    });
+}
+
+const viewAllEmployees = function () {
+  connection.query("SELECT * FROM employee", function (err, res) {
+    if (err) throw err;
+    console.log("----------------");
+    console.table(res);
+  });
+  initPrompt();
+};
+
+const viewAllDepartments = function () {
+  connection.query("SELECT * FROM department", function (err, res) {
+    if (err) throw err;
+    console.log("----------------");
+    console.table(res);
+  });
+  initPrompt();
+};
+
+const viewAllRoles = function () {
+  connection.query("SELECT * FROM role", function (err, res) {
+    if (err) throw err;
+    console.log("----------------");
+    console.table(res);
+  });
+  initPrompt();
+};
+
+const addEmployee = function () {
+  inquirer
+    .prompt([
+      {
+        name: "first_name",
+        type: "input",
+        message: "What is the first name of employee?",
+      },
+      {
+        name: "last_name",
+        type: "input",
+        message: "What is the last name of employee?",
+      },
+      {
+        name: "role_id",
+        type: "input",
+        message: "What is the role of the employee?",
+      },
+      {
+        name: "manager_id",
+        type: "input",
+        message: "Who is the manager of the employee?",
+      },
+    ])
+    .then(function (response) {
+      let sqlQuery = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUE ("${response.first_name}", "${response.last_name}", ${response.role_id}, ${response.manager_id})`;
+      connection.query(sqlQuery, function (err, res) {
+        if (err) throw err;
+        viewAllEmployees();
+      });
+    });
+};
+
+const addDepartment = function () {
+  inquirer
+    .prompt([
+      {
+        name: "name",
+        type: "input",
+        message: "What is the department name?",
+      },
+    ])
+    .then(function (response) {
+      let sqlQuery = `INSERT INTO department (name) VALUE ("${response.name}")`;
+      connection.query(sqlQuery, function (err, res) {
+        if (err) throw err;
+        viewAllDepartments();
+      });
+    });
+};
+
+const addRole = function () {
+  inquirer
+    .prompt([
+      {
+        name: "title",
+        type: "input",
+        message: "What is the new role?",
+      },
+      {
+        name: "salary",
+        type: "input",
+        message: "What is the salary of new role?",
+      },
+      {
+        name: "department_id",
+        type: "input",
+        message: "What is the department id of the new role?",
+      },
+    ])
+    .then(function (response) {
+      let sqlQuery = `INSERT INTO role (title, salary, department_id) VALUE ("${response.title}", ${response.salary}, "${response.department_id}")`;
+      connection.query(sqlQuery, function (err, res) {
+        if (err) throw err;
+        viewAllRoles();
+      });
+    });
+};
+
+const updateEmployee = function () {
+  connection.query("SELECT * FROM department", function (err, departments) {
+    if (err) throw err;
+    console.table(departments);
+    inquirer
+      .prompt([
+        {
+          name: "department",
+          type: "list",
+          message: "What department would you like to update?",
+          choices: departments.map((department) => ({
+            name: department.name,
+            value: department.id,
+          })),
+        },
+      ])
+      .then(function (response) {
+        console.log(response);
+      });
+  });
+};
